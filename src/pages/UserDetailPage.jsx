67262664b6c72c005d05cd2329297ac7330c404e
beloslav13 <velislav1329@gmail.com>
2024-10-17 21:00:48 +0300

    ПЕРВЫЙ УСПЕШНЫЙ PATCH !

--- src/pages/UserDetailPage.jsx
+++ src/pages/UserDetailPage.jsx
@@ -1,5 +1,5 @@
 import { useNavigate, useParams } from "react-router-dom";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Button, Card, Col, Form, Row } from "react-bootstrap";
 
@@ -21,9 +21,11 @@ function UserDetailPage() {
     username: '',
     first_name: ''
   });
+  const [patchFields, setPatchFields] = useState({})
   const [loading, setLoading] = useState(false);
   const [errors, setErrors] = useState({});
-  const { id } = useParams();
+  const [responseStatus, setResponseStatus] = useState({});
+  const {id} = useParams();
   const navigate = useNavigate();
 
   const goBack = () => navigate(-1);
@@ -37,15 +39,16 @@ function UserDetailPage() {
     .then(r => {
       setUser(r.data);
       setFields(r.data);
+      setResponseStatus(200)
     })
     .catch(err => console.log('error', err));
   }, [id]);
 
   const handleFields = (e) => {
-    const { name, value } = e.target;
+    const {name, value} = e.target;
 
     setErrors(prevErrors => {
-      const { [name]: _, ...newErrors } = prevErrors;
+      const {[name]: _, ...newErrors} = prevErrors;
       return newErrors;
     });
 
@@ -53,15 +56,33 @@ function UserDetailPage() {
       ...prevFields,
       [name]: value
     }));
+    setPatchFields(prevFields => ({
+      ...prevFields,
+      [name]: value
+    }));
   };
 
   const handlePatch = () => {
     setLoading(true);
-    // Добавьте логику для отправки данных на сервер
+    axios.patch(`http://127.0.0.1:8000/core/api/users/${id}/`, patchFields, {
+      headers: {
+        Authorization: `Token d7081e83fb1526a2e1dc92f21814d208282aaa49`
+      },
+    })
+    .then(res => {
+      console.log(res)
+    })
+    .catch(err => {
+      console.log(err)
+      setErrors(err)
+      setResponseStatus(err.status)
+    }).finally(() => setLoading(false));
   };
 
   console.log(user);
   console.log('fields', fields);
+  console.log('patchFields', patchFields);
+  console.log('errors', errors);
 
   return (
     <>
@@ -81,7 +102,7 @@ function UserDetailPage() {
                       <Form.Label>{label.username}</Form.Label>
                       <Form.Control
                         required
-                        name={label.username}
+                        name='username'
                         type="text"
                         placeholder="Введите..."
                         value={fields.username}
@@ -97,7 +118,7 @@ function UserDetailPage() {
                       <Form.Label>{label.first_name}</Form.Label>
                       <Form.Control
                         required
-                        name={label.first_name}
+                        name='first_name'
                         type="text"
                         placeholder="Введите..."
                         value={fields.first_name}
@@ -118,6 +139,15 @@ function UserDetailPage() {
                 </Row>
               </Form>
             </Card.Body>
+            <Card.Footer className='d-flex justify-content-center align-items-center'>
+              {responseStatus === 401 ? (
+                  <Form.Text
+                    className='alert alert-danger text-center mt-3 mb-3 fw-bold'>{errors.response.data.detail}</Form.Text>
+                ) :
+                errors.length > 0 ? (<Form.Text
+                    className='alert alert-danger text-center mt-3 mb-3 fw-bold'>Произошла ошибка при сохранение данных</Form.Text>)
+                  : null}
+            </Card.Footer>
           </Card>
         </Col>
         <Col sm={1} md={1} className='pt-5 m-auto'>
